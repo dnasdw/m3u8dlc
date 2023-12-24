cfc73313f3faeaef30790ebd6dc693a8074623d2
Sun Daowen <sundaowen@gmail.com>
2023-12-25 00:34:56 +0800

    添加libsundaowen

---
+++ src/m3u8dlc/libsundaowen/sdw_string.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace sdw
+{
+	public static partial class zzz
+	{
+		public static n8 SToN8(string a_sString, int a_nRadix = 10)
+		{
+			return (static_cast_n8)(Convert.ToInt32(a_sString, a_nRadix));
+		}
+
+		public static n16 SToN16(string a_sString, int a_nRadix = 10)
+		{
+			return (static_cast_n16)(Convert.ToInt32(a_sString, a_nRadix));
+		}
+
+		public static n32 SToN32(string a_sString, int a_nRadix = 10)
+		{
+			return (static_cast_n32)(Convert.ToInt32(a_sString, a_nRadix));
+		}
+
+		public static n64 SToN64(string a_sString, int a_nRadix = 10)
+		{
+			return Convert.ToInt64(a_sString, a_nRadix);
+		}
+
+		public static u8 SToU8(string a_sString, int a_nRadix = 10)
+		{
+			return (static_cast_u8)(Convert.ToUInt32(a_sString, a_nRadix));
+		}
+
+		public static u16 SToU16(string a_sString, int a_nRadix = 10)
+		{
+			return (static_cast_u16)(Convert.ToUInt32(a_sString, a_nRadix));
+		}
+
+		public static u32 SToU32(string a_sString, int a_nRadix = 10)
+		{
+			return (static_cast_u32)(Convert.ToUInt32(a_sString, a_nRadix));
+		}
+
+		public static u64 SToU64(string a_sString, int a_nRadix = 10)
+		{
+			return Convert.ToUInt64(a_sString, a_nRadix);
+		}
+
+		public static f32 SToF32(string a_sString)
+		{
+			return (static_cast_f32)(Convert.ToDouble(a_sString, CultureInfo.InvariantCulture));
+		}
+
+		public static f64 SToF64(string a_sString)
+		{
+			return Convert.ToDouble(a_sString, CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ src/m3u8dlc/libsundaowen/sdw_type.cs
@@ -0,0 +1,28 @@
+﻿// alias
+// typedef begin
+#pragma warning disable IDE0005
+global using n8 = System.SByte;
+global using n16 = System.Int16;
+global using n32 = System.Int32;
+global using n64 = System.Int64;
+global using u8 = System.Byte;
+global using u16 = System.UInt16;
+global using u32 = System.UInt32;
+global using u64 = System.UInt64;
+
+global using f32 = System.Single;
+global using f64 = System.Double;
+
+global using static_cast_n8 = System.SByte;
+global using static_cast_n16 = System.Int16;
+global using static_cast_n32 = System.Int32;
+global using static_cast_n64 = System.Int64;
+global using static_cast_u8 = System.Byte;
+global using static_cast_u16 = System.UInt16;
+global using static_cast_u32 = System.UInt32;
+global using static_cast_u64 = System.UInt64;
+
+global using static_cast_f32 = System.Single;
+global using static_cast_f64 = System.Double;
+#pragma warning restore IDE0005
+// typedef end
