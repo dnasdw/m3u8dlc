c54cb9488743e527e31b58685ef1db18b0f678f4
Sun Daowen <sundaowen@gmail.com>
2023-12-25 00:38:29 +0800

    配置Spectre.Console.Cli

--- src/m3u8dlc/Program.cs
+++ src/m3u8dlc/Program.cs
@@ -1,11 +1,46 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 
-namespace m3u8dlc;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
-class Program
+namespace m3u8dlc
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	internal class Program
+	{
+		public static async Task<n32> Main(string[] args)
+		{
+			CommandApp<AsyncMainCommand> app = new CommandApp<AsyncMainCommand>();
+			app.Configure(configuration);
+			n32 nResult = await app.RunAsync(args);
+			return nResult;
+		}
+
+		private static void configuration(IConfigurator config)
+		{
+			_ = config.SetExceptionHandler(exceptionHandler);
+			_ = config.SetApplicationName("m3u8dlc");
+			_ = config.UseStrictParsing();
+			_ = config.CaseSensitivity(CaseSensitivity.All);
+		}
+
+		private static n32 exceptionHandler(Exception ex)
+		{
+			AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+			return -1;
+		}
+	}
+
+	public class AsyncMainCommand : AsyncCommand<AsyncMainCommand.Settings>
+	{
+		public class Settings : CommandSettings
+		{
+		}
+
+		public override async Task<n32> ExecuteAsync([NotNull] CommandContext context, [NotNull] Settings settings)
+		{
+			return 0;
+		}
+	}
 }
