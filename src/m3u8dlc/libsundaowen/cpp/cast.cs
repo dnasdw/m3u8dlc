7ca6aa06324b4b0306896157aebb0f3d11a6c708
Sun Daowen <sundaowen@gmail.com>
2023-12-25 20:25:35 +0800

    更新libsundaowen

---
+++ src/m3u8dlc/libsundaowen/cpp/cast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace sdw
+{
+	public static partial class cpp
+	{
+		public static T? dynamic_cast<T>(object a_value) where T : class
+		{
+			return a_value as T;
+		}
+
+		public static T? reinterpret_cast<T>(object? a_value)
+		{
+			return (T?)a_value;
+		}
+
+		public static T? static_cast_slow<T>(object? a_value)
+		{
+			return (T?)Convert.ChangeType(a_value, typeof(T), CultureInfo.InvariantCulture);
+		}
+	}
+}
